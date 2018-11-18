164cd27de55524bd21b0255199ee542a47ee884d
Wouter Kenis <Da.wouter@gmail.com>
2018-11-18 13:34:00 +0100

    Added converter

---
+++ MusicStore/ConvertIntToString.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Data;
+
+namespace MusicStore
+{
+    public class ConvertIntToString : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            string format = value as string;
+
+            return "€" + format;
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            string format = value as string;
+
+            return Double.Parse(format.Substring(1));
+        }
+    }
+}
